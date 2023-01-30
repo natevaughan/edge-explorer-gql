639eecca50bfa1be03a62387a04d1af5be0619e1
Nate Vaughan <nate.vaughan@protonmail.com>
2023-01-29 23:49:40 -0600

    Use env variable for gql backend

--- util/apolloClient.ts
+++ util/apolloClient.ts
@@ -1,7 +1,7 @@
 import { ApolloClient, InMemoryCache } from "@apollo/client";
 
 const apolloClient = new ApolloClient({
-  uri: 'http://localhost:3001/api/graphql/',
+  uri: process.env.NEXT_PUBLIC_GRAPQL_ENTRY,
   cache: new InMemoryCache()
 });
 
